e41b779f15331ffb205ee3edf29e804831d7ccd9
Jesse van Woudenberg <jesse@vwoudenberg.com>
2022-11-22 16:16:59 +0100

    Nav W.I.P

--- resources/views/layouts/private-layout.blade.php
+++ resources/views/layouts/private-layout.blade.php
@@ -23,87 +23,143 @@
                 </div>
 
                 <ul>
-                    @can('article index')
-                        <li>
-                            <a href="{{ route('article') }}">Articles</a>
-                        </li>
-                    @endcan
+                    <li>
+                        <div class="private-nav-button-container">
+                            <button>
+                                <img class="icon-svg" src="{{ asset('resources/media/svgs/person-circle.svg') }}" alt="X">
+                                <span>FIA</span>
+                                <img class="arrow-down-svg" src="{{ asset('resources/media/svgs/arrow-down.svg') }}" alt="X">
+                            </button>
+                        </div>
 
-                    @can('constructorchampionship index')
-                        <li>
-                            <a href="{{ route('constructorchampionship') }}">Constructor Champs.</a>
-                        </li>
-                    @endcan
+                        <div class="private-nav-dropdown-content">
+                            <ul>
+                                @can('race index')
+                                    <li>
+                                        <a href="{{ route('race') }}">Races</a>
+                                    </li>
+                                @endcan
 
-                    @can('driverchampionship index')
-                        <li>
-                            <a href="{{ route('driverchampionship') }}">Driver Champs.</a>
-                        </li>
-                    @endcan
+                                @can('season index')
+                                    <li>
+                                        <a href="{{ route('season') }}">Seasons</a>
+                                    </li>
+                                @endcan
 
-                    @can('driver index')
-                        <li>
-                            <a href="{{ route('driver') }}">Drivers</a>
-                        </li>
-                    @endcan
-
-                    @can('powerunit index')
-                        <li>
-                            <a href="{{ route('powerunit') }}">Power-units</a>
-                        </li>
-                    @endcan
-
-                    @can('race index')
-                        <li>
-                            <a href="{{ route('race') }}">Races</a>
-                        </li>
-                    @endcan
-
-                    @can('season index')
-                        <li>
-                            <a href="{{ route('season') }}">Seasons</a>
-                        </li>
-                    @endcan
-
-                    @can('team index')
-                        <li>
-                            <a href="{{ route('team') }}">Teams</a>
-                        </li>
-                    @endcan
-
-                    @can('tier index')
-                        <li>
-                            <a href="{{ route('tier') }}">Tiers</a>
-                        </li>
-                    @endcan
-
-                    @can('track index')
-                        <li>
-                            <a href="{{ route('track') }}">Tracks</a>
-                        </li>
-                    @endcan
-
-                    @can('user index')
-                        <li>
-                            <a href="{{ route('user') }}">User</a>
-                        </li>
-                    @endcan
-
-                    @can('role index')
-                        <li>
-                            <a href="{{ route('role') }}">Role</a>
-                        </li>
-                    @endcan
-
-                    @can('penaltypoint index')
-                        <li>
-                            <a href="{{ route('penaltypoint') }}">Penalty Points</a>
-                        </li>
-                    @endcan
+                                @can('penaltypoint index')
+                                    <li>
+                                        <a href="{{ route('penaltypoint') }}">Penalty Points</a>
+                                    </li>
+                                @endcan
+                            </ul>
+                        </div>
+                    </li>
 
                     <li>
-                        <a href="{{ route('home') }}">Open page</a>
+                        <div class="private-nav-button-container">
+                            <button>
+                                <span>Admin</span>
+                                <img class="arrow-down-svg"  src="{{ asset('resources/media/svgs/arrow-down.svg') }}" alt="X">
+                            </button>
+                        </div>
+
+                        <div class="private-nav-dropdown-content">
+                            <ul>
+                                @can('user index')
+                                    <li>
+                                        <a href="{{ route('user') }}">User</a>
+                                    </li>
+                                @endcan
+
+                                @can('role index')
+                                    <li>
+                                        <a href="{{ route('role') }}">Role</a>
+                                    </li>
+                                @endcan
+                            </ul>
+                        </div>
                     </li>
+{{--                    @can('article index')--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ route('article') }}">Articles</a>--}}
+{{--                        </li>--}}
+{{--                    @endcan--}}
+
+{{--                    @can('constructorchampionship index')--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ route('constructorchampionship') }}">Constructor Champs.</a>--}}
+{{--                        </li>--}}
+{{--                    @endcan--}}
+
+{{--                    @can('driverchampionship index')--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ route('driverchampionship') }}">Driver Champs.</a>--}}
+{{--                        </li>--}}
+{{--                    @endcan--}}
+
+{{--                    @can('driver index')--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ route('driver') }}">Drivers</a>--}}
+{{--                        </li>--}}
+{{--                    @endcan--}}
+
+{{--                    @can('powerunit index')--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ route('powerunit') }}">Power-units</a>--}}
+{{--                        </li>--}}
+{{--                    @endcan--}}
+
+{{--                    @can('race index')--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ route('race') }}">Races</a>--}}
+{{--                        </li>--}}
+{{--                    @endcan--}}
+
+{{--                    @can('season index')--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ route('season') }}">Seasons</a>--}}
+{{--                        </li>--}}
+{{--                    @endcan--}}
+
+{{--                    @can('team index')--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ route('team') }}">Teams</a>--}}
+{{--                        </li>--}}
+{{--                    @endcan--}}
+
+{{--                    @can('tier index')--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ route('tier') }}">Tiers</a>--}}
+{{--                        </li>--}}
+{{--                    @endcan--}}
+
+{{--                    @can('track index')--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ route('track') }}">Tracks</a>--}}
+{{--                        </li>--}}
+{{--                    @endcan--}}
+
+{{--                    @can('user index')--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ route('user') }}">User</a>--}}
+{{--                        </li>--}}
+{{--                    @endcan--}}
+
+{{--                    @can('role index')--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ route('role') }}">Role</a>--}}
+{{--                        </li>--}}
+{{--                    @endcan--}}
+
+{{--                    @can('penaltypoint index')--}}
+{{--                        <li>--}}
+{{--                            <a href="{{ route('penaltypoint') }}">Penalty Points</a>--}}
+{{--                        </li>--}}
+{{--                    @endcan--}}
+
+{{--                    <li>--}}
+{{--                        <a href="{{ route('home') }}">Open page</a>--}}
+{{--                    </li>--}}
 
                 </ul>
             </nav>
