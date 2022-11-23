9361133dee3ae4b39a4f5b3c9ab7e0e211895973
Jesse van Woudenberg <jesse@vwoudenberg.com>
2022-11-23 16:11:23 +0100

    Private Nav Redesign  + svg's added

--- public/resources/javascript/app.js
+++ public/resources/javascript/app.js
@@ -2,6 +2,18 @@ const menubutton = document.querySelector('.menu-button');
 const nav = document.querySelector('.private-navigation');
 const main = document.querySelector('.main-container');
 
+const adminButton = document.getElementById('admin-button');
+const adminDropdown = document.getElementById('admin-dropdown');
+
+const fiaButton = document.getElementById('fia-button');
+const fiaDropdown = document.getElementById('fia-dropdown');
+
+const reporterButton = document.getElementById('reporter-button');
+const reporterDropdown = document.getElementById('reporter-dropdown');
+
+const championshipsButton = document.getElementById('championships-button');
+const championshipsDropdown = document.getElementById('championships-dropdown');
+
 const navFunctionality = () => {
 
     if(localStorage.getItem('menu-open') === "true") {
@@ -31,10 +43,116 @@ const navFunctionality = () => {
     });
 }
 
-const standingsFunctionality = () => {
-
+const collapseAllCategories = () => {
+    adminDropdown.classList.remove('activated');
+    fiaDropdown.classList.remove('activated');
+    reporterDropdown.classList.remove('activated');
+    championshipsDropdown.classList.remove('activated');
 }
 
-standingsFunctionality()
+const navCategoriesFunctionality = () => {
+
+    adminButton.addEventListener('click', () => {
+        let active = adminDropdown.classList.contains('activated');
+
+        collapseAllCategories();
+
+        if (!active) {
+            adminDropdown.classList.add('activated');
+        }
+    })
+
+    fiaButton.addEventListener('click', () => {
+        let active = fiaDropdown.classList.contains('activated');
+
+        collapseAllCategories();
+
+        if (!active) {
+            fiaDropdown.classList.add('activated');
+        }
+    })
+
+    reporterButton.addEventListener('click', () => {
+        let active = reporterDropdown.classList.contains('activated');
+
+        collapseAllCategories();
+
+        if (!active) {
+            reporterDropdown.classList.add('activated');
+        }
+    })
+
+    championshipsButton.addEventListener('click', () => {
+        let active = championshipsDropdown.classList.contains('activated');
+
+        collapseAllCategories();
+
+        if (!active) {
+            championshipsDropdown.classList.add('activated');
+        }
+    })
+
+    // adminDropdown.style.display = 'none';
+    //
+    // adminButton.addEventListener('click', ()=> {
+    //
+    //     if (adminDropdown.style.display === 'none') {
+    //
+    //         collapseAllCategories();
+    //         adminDropdown.style.display = 'block';
+    //
+    //     } else {
+    //
+    //         adminDropdown.style.display = 'none';
+    //
+    //     }
+    //
+    // })
+    //
+    // fiaDropdown.style.display = 'none';
+    //
+    // fiaButton.addEventListener('click', ()=> {
+    //
+    //     if (fiaDropdown.style.display === 'none') {
+    //
+    //         collapseAllCategories();
+    //         fiaDropdown.style.display = 'block';
+    //
+    //     } else {
+    //
+    //         fiaDropdown.style.display = 'none';
+    //
+    //     }
+    // })
+    //
+    // // Reporter button functionality
+    // reporterDropdown.style.display = 'none';
+    //
+    // reporterButton.addEventListener('click', ()=> {
+    //
+    //     collapseAllCategories();
+    //     reporterDropdown.classList.toggle('activated');
+    //
+    // })
+    //
+    // // Championships button functionality
+    // championshipsDropdown.style.display = 'none';
+    //
+    // championshipsButton.addEventListener('click', ()=> {
+    //
+    //     if (championshipsDropdown.style.display === 'none') {
+    //
+    //         collapseAllCategories();
+    //         championshipsDropdown.style.display = 'block';
+    //
+    //     } else {
+    //
+    //         championshipsDropdown.style.display = 'none';
+    //
+    //     }
+    // })
+}
+
+navCategoriesFunctionality()
 navFunctionality();
 
--- resources/views/layouts/private-layout.blade.php
+++ resources/views/layouts/private-layout.blade.php
@@ -22,27 +22,72 @@
                     <div class="line3"></div>
                 </div>
 
-                <ul>
+                <ul class="private-nav-list">
+
+                    <li>
+                        <a href="{{ route('admin') }}">Home</a>
+                    </li>
+
                     <li>
                         <div class="private-nav-button-container">
-                            <button>
-                                <img class="icon-svg" src="{{ asset('resources/media/svgs/person-circle.svg') }}" alt="X">
+                            <button id="reporter-button">
+                                <img class="icon-svg" src="{{ asset('resources/media/svgs/book-fill.svg') }}" alt="X">
+                                <span>Media</span>
+                                <img class="arrow-down-svg" src="{{ asset('resources/media/svgs/arrow-down.svg') }}" alt="X">
+                            </button>
+                        </div>
+
+                        <div class="private-nav-dropdown-content" id="reporter-dropdown">
+                            <ul>
+                                @can('article index')
+                                    <li>
+                                        <a href="{{ route('article') }}">Articles</a>
+                                    </li>
+                                @endcan
+                            </ul>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="private-nav-button-container">
+                            <button id="championships-button">
+                                <img class="icon-svg" src="{{ asset('resources/media/svgs/trophy-fill.svg') }}" alt="X">
+                                <span>Championships</span>
+                                <img class="arrow-down-svg" src="{{ asset('resources/media/svgs/arrow-down.svg') }}" alt="X">
+                            </button>
+                        </div>
+
+                        <div class="private-nav-dropdown-content" id="championships-dropdown">
+                            <ul>
+                                @can('constructorchampionship index')
+                                    <li>
+                                        <a href="{{ route('constructorchampionship') }}">Constructor Champs.</a>
+                                    </li>
+                                @endcan
+
+                                @can('driverchampionship index')
+                                    <li>
+                                        <a href="{{ route('driverchampionship') }}">Driver Champs.</a>
+                                    </li>
+                                @endcan
+                            </ul>
+                        </div>
+                    </li>
+
+                    <li>
+                        <div class="private-nav-button-container">
+                            <button id="fia-button">
+                                <img class="icon-svg" src="{{ asset('resources/media/svgs/flag-fill.svg') }}" alt="X">
                                 <span>FIA</span>
                                 <img class="arrow-down-svg" src="{{ asset('resources/media/svgs/arrow-down.svg') }}" alt="X">
                             </button>
                         </div>
 
-                        <div class="private-nav-dropdown-content">
+                        <div class="private-nav-dropdown-content" id="fia-dropdown">
                             <ul>
                                 @can('race index')
                                     <li>
                                         <a href="{{ route('race') }}">Races</a>
-                                    </li>
-                                @endcan
-
-                                @can('season index')
-                                    <li>
-                                        <a href="{{ route('season') }}">Seasons</a>
                                     </li>
                                 @endcan
 
@@ -57,14 +102,52 @@
 
                     <li>
                         <div class="private-nav-button-container">
-                            <button>
+                            <button id="admin-button">
+                                <img class="icon-svg" src="{{ asset('resources/media/svgs/lock-fill.svg') }}" alt="X">
                                 <span>Admin</span>
                                 <img class="arrow-down-svg"  src="{{ asset('resources/media/svgs/arrow-down.svg') }}" alt="X">
                             </button>
                         </div>
 
-                        <div class="private-nav-dropdown-content">
+                        <div class="private-nav-dropdown-content" id="admin-dropdown">
                             <ul>
+
+                                @can('driver index')
+                                    <li>
+                                        <a href="{{ route('driver') }}">Drivers</a>
+                                    </li>
+                                @endcan
+
+                                @can('team index')
+                                    <li>
+                                        <a href="{{ route('team') }}">Teams</a>
+                                    </li>
+                                @endcan
+
+                                @can('season index')
+                                    <li>
+                                        <a href="{{ route('season') }}">Seasons</a>
+                                    </li>
+                                @endcan
+
+                                @can('tier index')
+                                    <li>
+                                        <a href="{{ route('tier') }}">Tiers</a>
+                                    </li>
+                                @endcan
+
+                                @can('track index')
+                                    <li>
+                                        <a href="{{ route('track') }}">Tracks</a>
+                                    </li>
+                                @endcan
+
+                                @can('powerunit index')
+                                    <li>
+                                        <a href="{{ route('powerunit') }}">Power-units</a>
+                                    </li>
+                                @endcan
+
                                 @can('user index')
                                     <li>
                                         <a href="{{ route('user') }}">User</a>
@@ -79,90 +162,12 @@
                             </ul>
                         </div>
                     </li>
-{{--                    @can('article index')--}}
-{{--                        <li>--}}
-{{--                            <a href="{{ route('article') }}">Articles</a>--}}
-{{--                        </li>--}}
-{{--                    @endcan--}}
-
-{{--                    @can('constructorchampionship index')--}}
-{{--                        <li>--}}
-{{--                            <a href="{{ route('constructorchampionship') }}">Constructor Champs.</a>--}}
-{{--                        </li>--}}
-{{--                    @endcan--}}
-
-{{--                    @can('driverchampionship index')--}}
-{{--                        <li>--}}
-{{--                            <a href="{{ route('driverchampionship') }}">Driver Champs.</a>--}}
-{{--                        </li>--}}
-{{--                    @endcan--}}
-
-{{--                    @can('driver index')--}}
-{{--                        <li>--}}
-{{--                            <a href="{{ route('driver') }}">Drivers</a>--}}
-{{--                        </li>--}}
-{{--                    @endcan--}}
-
-{{--                    @can('powerunit index')--}}
-{{--                        <li>--}}
-{{--                            <a href="{{ route('powerunit') }}">Power-units</a>--}}
-{{--                        </li>--}}
-{{--                    @endcan--}}
-
-{{--                    @can('race index')--}}
-{{--                        <li>--}}
-{{--                            <a href="{{ route('race') }}">Races</a>--}}
-{{--                        </li>--}}
-{{--                    @endcan--}}
-
-{{--                    @can('season index')--}}
-{{--                        <li>--}}
-{{--                            <a href="{{ route('season') }}">Seasons</a>--}}
-{{--                        </li>--}}
-{{--                    @endcan--}}
-
-{{--                    @can('team index')--}}
-{{--                        <li>--}}
-{{--                            <a href="{{ route('team') }}">Teams</a>--}}
-{{--                        </li>--}}
-{{--                    @endcan--}}
-
-{{--                    @can('tier index')--}}
-{{--                        <li>--}}
-{{--                            <a href="{{ route('tier') }}">Tiers</a>--}}
-{{--                        </li>--}}
-{{--                    @endcan--}}
-
-{{--                    @can('track index')--}}
-{{--                        <li>--}}
-{{--                            <a href="{{ route('track') }}">Tracks</a>--}}
-{{--                        </li>--}}
-{{--                    @endcan--}}
-
-{{--                    @can('user index')--}}
-{{--                        <li>--}}
-{{--                            <a href="{{ route('user') }}">User</a>--}}
-{{--                        </li>--}}
-{{--                    @endcan--}}
-
-{{--                    @can('role index')--}}
-{{--                        <li>--}}
-{{--                            <a href="{{ route('role') }}">Role</a>--}}
-{{--                        </li>--}}
-{{--                    @endcan--}}
-
-{{--                    @can('penaltypoint index')--}}
-{{--                        <li>--}}
-{{--                            <a href="{{ route('penaltypoint') }}">Penalty Points</a>--}}
-{{--                        </li>--}}
-{{--                    @endcan--}}
-
-{{--                    <li>--}}
-{{--                        <a href="{{ route('home') }}">Open page</a>--}}
-{{--                    </li>--}}
-
                 </ul>
             </nav>
+
+            <div class="open-page-link">
+                <a href="{{ route('home') }}">Open page</a>
+            </div>
         </header>
 
         <main class="main-container">
